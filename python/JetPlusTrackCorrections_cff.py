e65f14f87206c16ca7d9dc1531b81d0332627196
cmsbuild <>
2009-08-05 11:23:30 +0000

    This commit was manufactured by cvs2git to create tag 'JetMETCorrections- Configuration-V01-08-20'.
    Sprout from master 2009-08-05 11:23:29 UTC Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch> 'Updating the trigger names'
    Cherrypick from master 2009-03-30 15:55:05 UTC Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch> 'refactorized python fragments to suit better the new implemntation of JPT in the PAT':
        python/JetPlusTrackCorrections_cff.py

--- python/JetPlusTrackCorrections_cff.py
+++ python/JetPlusTrackCorrections_cff.py
@@ -11,54 +11,6 @@ from RecoJets.Configuration.RecoJetAssociations_cff import *
 
 from JetMETCorrections.Configuration.JetPlusTrackCorrections_cfi import *
 
-#---------- Electron ID
-#for 22x
-from RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi import *
-## eIDRobustLoose = eidCutBasedExt.clone()
-## eIDRobustLoose.electronQuality = 'robust'
-
-## eIDRobustTight = eidCutBasedExt.clone()
-## eIDRobustTight.electronQuality = 'robust'
-## eIDRobustTight.robustEleIDCuts.barrel = [0.015, 0.0092, 0.020, 0.0025]
-## eIDRobustTight.robustEleIDCuts.endcap = [0.018, 0.025, 0.020, 0.0040]
-## eIDRobustHighEnergy = eidCutBasedExt.clone()
-## eIDRobustHighEnergy.electronQuality = 'robust'
-## eIDRobustHighEnergy.robustEleIDCuts.barrel = [0.050, 0.011, 0.090, 0.005]
-## eIDRobustHighEnergy.robustEleIDCuts.endcap = [0.100, 0.0275, 0.090, 0.007]
-
-## eIDLoose = eidCutBasedExt.clone()
-## eIDLoose.electronQuality = 'loose'
-
-## eIDTight = eidCutBasedExt.clone()
-## eIDTight.electronQuality = 'loose'
-
-## eIdSequence = cms.Sequence(eIDRobustLoose+eIDRobustTight+eIDRobustHighEnergy+eIDLoose+eIDTight)
-
-#from RecoEgamma.ElectronIdentification.electronIdSequence_cfi import *
-eidRobustLoose = eidCutBasedExt.clone()
-eidRobustLoose.electronIDType = 'robust'
-eidRobustLoose.electronQuality = 'loose'
-
-eidRobustTight = eidCutBasedExt.clone()
-eidRobustTight.electronIDType = 'robust'
-eidRobustTight.electronQuality = 'tight'
-
-eidRobustHighEnergy = eidCutBasedExt.clone()
-eidRobustHighEnergy.electronIDType = 'robust'
-eidRobustHighEnergy.electronQuality = 'highenergy'
-
-eidLoose = eidCutBasedExt.clone()
-eidLoose.electronIDType = 'classbased'
-eidLoose.electronQuality = 'loose'
-
-eidTight = eidCutBasedExt.clone()
-eidTight.electronIDType = 'classbased'
-eidTight.electronQuality = 'tight'
-
-eIdSequence = cms.Sequence(eidRobustLoose+eidRobustTight+eidRobustHighEnergy+eidLoose+eidTight)
-#-----------
-
-
 JetPlusTrackZSPCorrectorIcone5 = cms.ESSource(
     "JetPlusTrackCorrectionService",
     JPTZSPCorrectorICone5,
@@ -85,7 +37,7 @@ ZSPiterativeCone5JetExtender.jet2TracksAtCALO = cms.InputTag("ZSPiterativeCone5J
 ZSPiterativeCone5JetExtender.jet2TracksAtVX = cms.InputTag("ZSPiterativeCone5JetTracksAssociatorAtVertex")
 
 
-ZSPrecoJetAssociations = cms.Sequence(eIdSequence*ZSPiterativeCone5JetTracksAssociatorAtVertex*ZSPiterativeCone5JetTracksAssociatorAtCaloFace*ZSPiterativeCone5JetExtender)
+ZSPrecoJetAssociations = cms.Sequence(ZSPiterativeCone5JetTracksAssociatorAtVertex*ZSPiterativeCone5JetTracksAssociatorAtCaloFace*ZSPiterativeCone5JetExtender)
 
 JetPlusTrackCorrections = cms.Sequence(ZSPrecoJetAssociations*JetPlusTrackZSPCorJetIcone5)
 
