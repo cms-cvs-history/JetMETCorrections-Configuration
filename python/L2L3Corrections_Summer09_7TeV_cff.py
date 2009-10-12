760e04f87d50d1e714c2ed819e7a58dc830af7e4
cmsbuild <>
2009-10-12 15:17:34 +0000

    This commit was manufactured by cvs2git to create tag 'JetMETCorrections- Configuration-CMSSW_3_3_3_TSG'.
    Sprout from master 2009-09-13 16:38:58 UTC Olga Kodolova <sha1-ea692fd16269a55c9895f16ae2b67a003b603c27@cern.ch> '*** empty log message ***'
    Cherrypick from master 2009-10-12 15:17:32 UTC Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch> 'Changed the antikt name to ak. Consistent with CMSSW 330.':
        python/L2L3Corrections_Summer09_7TeV_cff.py
        python/L2L3Corrections_Summer09_cff.py

--- python/L2L3Corrections_Summer09_7TeV_cff.py
+++ python/L2L3Corrections_Summer09_7TeV_cff.py
@@ -2,100 +2,116 @@ import FWCore.ParameterSet.Config as cms
 
 #################### L2 Source definitions ##################################
 L2JetCorrectorIC5Calo = cms.ESSource("L2RelativeCorrectionService", 
-    tagName = cms.string('Summer09_L2Relative_IC5Calo'),
+    tagName = cms.string('Summer09_7TeV_L2Relative_IC5Calo'),
     label = cms.string('L2RelativeJetCorrectorIC5Calo')
 )
 L2JetCorrectorIC5PF= cms.ESSource("L2RelativeCorrectionService", 
-    tagName = cms.string('Summer09_L2Relative_IC5PF'),
+    tagName = cms.string('Summer09_7TeV_L2Relative_IC5PF'),
     label = cms.string('L2RelativeJetCorrectorIC5PF')
 )
 L2JetCorrectorAK5Calo = cms.ESSource("L2RelativeCorrectionService", 
-    tagName = cms.string('Summer09_L2Relative_AK5Calo'),
+    tagName = cms.string('Summer09_7TeV_L2Relative_AK5Calo'),
     label = cms.string('L2RelativeJetCorrectorAK5Calo')
 )
 L2JetCorrectorAK5PF= cms.ESSource("L2RelativeCorrectionService", 
-    tagName = cms.string('Summer09_L2Relative_AK5PF'),
+    tagName = cms.string('Summer09_7TeV_L2Relative_AK5PF'),
     label = cms.string('L2RelativeJetCorrectorAK5PF')
 )
+L2JetCorrectorAK7Calo = cms.ESSource("L2RelativeCorrectionService", 
+    tagName = cms.string('Summer09_7TeV_L2Relative_AK7Calo'),
+    label = cms.string('L2RelativeJetCorrectorAK7Calo')
+)
+L2JetCorrectorAK7PF= cms.ESSource("L2RelativeCorrectionService", 
+    tagName = cms.string('Summer09_7TeV_L2Relative_AK7PF'),
+    label = cms.string('L2RelativeJetCorrectorAK7PF')
+)
 L2JetCorrectorSC5Calo = cms.ESSource("L2RelativeCorrectionService", 
-    tagName = cms.string('Summer09_L2Relative_SC5Calo'),
+    tagName = cms.string('Summer09_7TeV_L2Relative_SC5Calo'),
     label = cms.string('L2RelativeJetCorrectorSC5Calo')
 )
 L2JetCorrectorSC5PF= cms.ESSource("L2RelativeCorrectionService", 
-    tagName = cms.string('Summer09_L2Relative_SC5PF'),
+    tagName = cms.string('Summer09_7TeV_L2Relative_SC5PF'),
     label = cms.string('L2RelativeJetCorrectorSC5PF')
 )
 L2JetCorrectorSC7Calo = cms.ESSource("L2RelativeCorrectionService", 
-    tagName = cms.string('Summer09_L2Relative_SC7Calo'),
+    tagName = cms.string('Summer09_7TeV_L2Relative_SC7Calo'),
     label = cms.string('L2RelativeJetCorrectorSC7Calo')
 )
 L2JetCorrectorSC7PF= cms.ESSource("L2RelativeCorrectionService", 
-    tagName = cms.string('Summer09_L2Relative_SC7PF'),
+    tagName = cms.string('Summer09_7TeV_L2Relative_SC7PF'),
     label = cms.string('L2RelativeJetCorrectorSC7PF')
 )
 L2JetCorrectorKT4Calo = cms.ESSource("L2RelativeCorrectionService", 
-    tagName = cms.string('Summer09_L2Relative_KT4Calo'),
+    tagName = cms.string('Summer09_7TeV_L2Relative_KT4Calo'),
     label = cms.string('L2RelativeJetCorrectorKT4Calo')
 )
 L2JetCorrectorKT4PF = cms.ESSource("L2RelativeCorrectionService", 
-    tagName = cms.string('Summer09_L2Relative_KT4PF'),
+    tagName = cms.string('Summer09_7TeV_L2Relative_KT4PF'),
     label = cms.string('L2RelativeJetCorrectorKT4PF')
 )
 L2JetCorrectorKT6Calo = cms.ESSource("L2RelativeCorrectionService", 
-    tagName = cms.string('Summer09_L2Relative_KT6Calo'),
+    tagName = cms.string('Summer09_7TeV_L2Relative_KT6Calo'),
     label = cms.string('L2RelativeJetCorrectorKT6Calo')
 )
 L2JetCorrectorKT6PF = cms.ESSource("L2RelativeCorrectionService", 
-    tagName = cms.string('Summer09_L2Relative_KT6PF'),
+    tagName = cms.string('Summer09_7TeV_L2Relative_KT6PF'),
     label = cms.string('L2RelativeJetCorrectorKT6PF')
 )
 #################### L3 Source definitions ##################################
 L3JetCorrectorIC5Calo = cms.ESSource("L3AbsoluteCorrectionService", 
-    tagName = cms.string('Summer09_L3Absolute_IC5Calo'),
+    tagName = cms.string('Summer09_7TeV_L3Absolute_IC5Calo'),
     label = cms.string('L3AbsoluteJetCorrectorIC5Calo')
 )
 L3JetCorrectorIC5PF = cms.ESSource("L3PFAbsoluteCorrectionService", 
-    tagName = cms.string('Summer09_L3Absolute_IC5PF'),
+    tagName = cms.string('Summer09_7TeV_L3Absolute_IC5PF'),
     label = cms.string('L3AbsoluteJetCorrectorIC5PF')
 )
 L3JetCorrectorAK5Calo = cms.ESSource("L3AbsoluteCorrectionService", 
-    tagName = cms.string('Summer09_L3Absolute_AK5Calo'),
+    tagName = cms.string('Summer09_7TeV_L3Absolute_AK5Calo'),
     label = cms.string('L3AbsoluteJetCorrectorAK5Calo')
 )
 L3JetCorrectorAK5PF = cms.ESSource("L3PFAbsoluteCorrectionService", 
-    tagName = cms.string('Summer09_L3Absolute_AK5PF'),
+    tagName = cms.string('Summer09_7TeV_L3Absolute_AK5PF'),
     label = cms.string('L3AbsoluteJetCorrectorAK5PF')
 )
+L3JetCorrectorAK7Calo = cms.ESSource("L3AbsoluteCorrectionService", 
+    tagName = cms.string('Summer09_7TeV_L3Absolute_AK7Calo'),
+    label = cms.string('L3AbsoluteJetCorrectorAK7Calo')
+)
+L3JetCorrectorAK7PF = cms.ESSource("L3PFAbsoluteCorrectionService", 
+    tagName = cms.string('Summer09_7TeV_L3Absolute_AK7PF'),
+    label = cms.string('L3AbsoluteJetCorrectorAK7PF')
+)
 L3JetCorrectorSC5Calo = cms.ESSource("L3AbsoluteCorrectionService", 
-    tagName = cms.string('Summer09_L3Absolute_SC5Calo'),
+    tagName = cms.string('Summer09_7TeV_L3Absolute_SC5Calo'),
     label = cms.string('L3AbsoluteJetCorrectorSC5Calo')
 )
 L3JetCorrectorSC5PF = cms.ESSource("L3PFAbsoluteCorrectionService", 
-    tagName = cms.string('Summer09_L3Absolute_SC5PF'),
+    tagName = cms.string('Summer09_7TeV_L3Absolute_SC5PF'),
     label = cms.string('L3AbsoluteJetCorrectorSC5PF')
 )
 L3JetCorrectorSC7Calo = cms.ESSource("L3AbsoluteCorrectionService", 
-    tagName = cms.string('Summer09_L3Absolute_SC7Calo'),
+    tagName = cms.string('Summer09_7TeV_L3Absolute_SC7Calo'),
     label = cms.string('L3AbsoluteJetCorrectorSC7Calo')
 )
 L3JetCorrectorSC7PF = cms.ESSource("L3PFAbsoluteCorrectionService", 
-    tagName = cms.string('Summer09_L3Absolute_SC7PF'),
+    tagName = cms.string('Summer09_7TeV_L3Absolute_SC7PF'),
     label = cms.string('L3AbsoluteJetCorrectorSC7PF')
 )
 L3JetCorrectorKT4Calo = cms.ESSource("L3AbsoluteCorrectionService", 
-    tagName = cms.string('Summer09_L3Absolute_KT4Calo'),
+    tagName = cms.string('Summer09_7TeV_L3Absolute_KT4Calo'),
     label = cms.string('L3AbsoluteJetCorrectorKT4Calo')
 )
 L3JetCorrectorKT4PF = cms.ESSource("L3PFAbsoluteCorrectionService", 
-    tagName = cms.string('Summer09_L3Absolute_KT4PF'),
+    tagName = cms.string('Summer09_7TeV_L3Absolute_KT4PF'),
     label = cms.string('L3AbsoluteJetCorrectorKT4PF')
 )
 L3JetCorrectorKT6Calo = cms.ESSource("L3AbsoluteCorrectionService", 
-    tagName = cms.string('Summer09_L3Absolute_KT6Calo'),
+    tagName = cms.string('Summer09_7TeV_L3Absolute_KT6Calo'),
     label = cms.string('L3AbsoluteJetCorrectorKT6Calo')
 )
 L3JetCorrectorKT6PF = cms.ESSource("L3PFAbsoluteCorrectionService", 
-    tagName = cms.string('Summer09_L3Absolute_KT6PF'),
+    tagName = cms.string('Summer09_7TeV_L3Absolute_KT6PF'),
     label = cms.string('L3AbsoluteJetCorrectorKT6PF')
 )
 #################### L2L3 Source definitions ##################################
@@ -114,6 +130,14 @@ L2L3JetCorrectorAK5Calo = cms.ESSource("JetCorrectionServiceChain",
 L2L3JetCorrectorAK5PF = cms.ESSource("JetCorrectionServiceChain",  
     correctors = cms.vstring('L2RelativeJetCorrectorAK5PF','L3AbsoluteJetCorrectorAK5PF'),
     label = cms.string('L2L3JetCorrectorAK5PF') 
+)
+L2L3JetCorrectorAK7Calo = cms.ESSource("JetCorrectionServiceChain",  
+    correctors = cms.vstring('L2RelativeJetCorrectorAK7Calo','L3AbsoluteJetCorrectorAK7Calo'),
+    label = cms.string('L2L3JetCorrectorAK7Calo') 
+)
+L2L3JetCorrectorAK7PF = cms.ESSource("JetCorrectionServiceChain",  
+    correctors = cms.vstring('L2RelativeJetCorrectorAK7PF','L3AbsoluteJetCorrectorAK7PF'),
+    label = cms.string('L2L3JetCorrectorAK7PF') 
 )
 L2L3JetCorrectorSC5Calo = cms.ESSource("JetCorrectionServiceChain",  
     correctors = cms.vstring('L2RelativeJetCorrectorSC5Calo','L3AbsoluteJetCorrectorSC5Calo'),
@@ -163,6 +187,14 @@ L2L3CorJetAK5Calo = cms.EDProducer("CaloJetCorrectionProducer",
 L2L3CorJetAK5PF = cms.EDProducer("PFJetCorrectionProducer",
     src = cms.InputTag("ak5PFJets"),
     correctors = cms.vstring('L2L3JetCorrectorAK5PF')
+)
+L2L3CorJetAK7Calo = cms.EDProducer("CaloJetCorrectionProducer",
+    src = cms.InputTag("ak7CaloJets"),
+    correctors = cms.vstring('L2L3JetCorrectorAK7Calo')
+)
+L2L3CorJetAK7PF = cms.EDProducer("PFJetCorrectionProducer",
+    src = cms.InputTag("ak7PFJets"),
+    correctors = cms.vstring('L2L3JetCorrectorAK7PF')
 )
 L2L3CorJetSC5Calo = cms.EDProducer("CaloJetCorrectionProducer",
     src = cms.InputTag("sisCone5CaloJets"),
